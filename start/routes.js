7c03f5462439e83a07670050ee4c6dc43ba99f7d
ammar ahmed <aammarr.ahmed@gmail.com>
2020-08-27 02:22:53 +0500

    Add Api & Web routes

---
+++ start/routes.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+/**
+ * Testing Routes
+ */
+Route.on('/').render('welcome')
+
+
+/**
+ * Api Routes
+ */
+Route.group(() => {  
+
+    Route.get('/test-api-route','Api/TestApiController.index')
+
+}).prefix('api/v1')
+
+
+
+ /**
+ * Web Routes
+ */
